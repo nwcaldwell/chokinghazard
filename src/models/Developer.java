11520ba326cdf75542f2a108bea4055877653cf0
Meghan King <meghantking67@gmail.com>
2014-02-09 14:27:49 -0500

    Changed Developer serialize() method to not have infinite loops when loading. Will probably be modified with the structural changes being done

--- src/models/Developer.java
+++ src/models/Developer.java
@@ -30,10 +30,23 @@ class Developer implements Serializable<Developer>{
   }
 
   public String serialize() {
+	  /*This creates a string that represents a Developer object for saving and loading
+	   *
+	   * The boolean isPlacedOnBoard is stored. toString() for a boolean either returns "True" or "False"
+	   * so it needs to be convered to "true" and "false" so that loading is easier.
+	   * 
+	   * currentCell will be saved using it's (x,y) coordinatesm in currentCellX and currentCellY .
+	   *  This will make the Cell unique
+	   * 
+	   * The owner should (I hope) be unique by the player's color, so that will be saved this way..
+	   * We may not even need to save this, since the Developer should be made with the corresponding
+	   * Player who owns it... anyways :P
+	   */
 	    return Json.jsonPair("Developer", Json.jsonObject(Json.jsonMembers(
 	    		Json.jsonPair("isPlacedOnBoard", Json.jsonValue(isPlacedOnBoard.toString().toLower())),
-	    		currentCell.serialize(), 
-	    		owner.serialize()
+	    		Json.jsonPair("currentCellX", Json.jsonValue(currentCell.getX() + "")), 
+	    		Json.jsonPair("currentCellY", Json.jsonValue(currentCell.getY() + "")),
+	    		Json.jsonPair("ownerColor", Json.jsonValue(owner.getColor().toString()))
 	    		)));
   }
 
